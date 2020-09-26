06535dffabf2cafd0115348665b56e4ffbd341d9
MFA <53713274+mfabank@users.noreply.github.com>
2020-09-27 00:33:29 +0300

    Firebase done !

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyApp(),
-    ));
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -14,74 +18,110 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String input;
   List todos = List();
+
+  createTodos() {
+    DocumentReference documentReference =
+        Firestore.instance.collection("MyTodos").document(input);
+
+    Map<String, String> todos = {"todoTitle": input};
+    documentReference
+        .setData(todos)
+        .whenComplete(() => {print("$input created")});
+  }
+
+  deleteTodos(item) {
+    DocumentReference documentReference =
+        Firestore.instance.collection("MyTodos").document(item);
+    documentReference.delete().whenComplete(() => print("deleted"));
+  }
+
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        print("çalıştı");
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        print("hata var");
+      });
+    }
+  }
+
   @override
   void initState() {
-    // TODO: implement initState
+    initializeFlutterFire();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepOrangeAccent[100],
-        title: Text("Yapılacaklar Listesi"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.deepOrangeAccent[100],
-        child: Icon(Icons.add),
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text("Görev Ekle"),
-                  content: TextField(
-                    onChanged: (String value) {
-                      input = value;
-                    },
-                  ),
-                  actions: [
-                    FlatButton(
-                      child: Text("Ekle"),
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrangeAccent[100],
+          title: Text("Yapılacaklar Listesi"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.deepOrangeAccent[100],
+          child: Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("Görev Ekle"),
+                    content: TextField(
+                      onChanged: (String value) {
+                        input = value;
+                      },
+                    ),
+                    actions: [
+                      FlatButton(
+                        child: Text("Ekle"),
+                        onPressed: () {
+                          createTodos();
+
+                          Navigator.of(context).pop();
+                        },
+                      )
+                    ],
+                  );
+                });
+          },
+        ),
+        body: StreamBuilder(
+          stream: Firestore.instance.collection("MyTodos").snapshots(),
+          builder: (context, snapshots) {
+            return ListView.builder(
+              itemCount: snapshots.data.documents.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot documentSnapshot =
+                    snapshots.data.documents[index];
+
+                return Card(
+                  elevation: 8,
+                  margin: EdgeInsets.all(8),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: ListTile(
+                    title: Text(documentSnapshot.data()["todoTitle"]),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Colors.red,
                       onPressed: () {
                         setState(() {
-                          todos.add(input);
+                          deleteTodos(documentSnapshot.data()["todoTitle"]);
                         });
-                        Navigator.of(context).pop();
                       },
-                    )
-                  ],
+                    ),
+                  ),
                 );
-              });
-        },
-      ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          return Dismissible(
-            key: Key(todos[index]),
-            child: Card(
-              elevation: 8,
-              margin: EdgeInsets.all(8),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              child: ListTile(
-                title: Text(todos[index]),
-                trailing: IconButton(
-                  icon: Icon(Icons.delete),
-                  color: Colors.red,
-                  onPressed: () {
-                    setState(() {
-                      todos.removeAt(index);
-                    });
-                  },
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
+              },
+            );
+          },
+        ));
   }
 }
